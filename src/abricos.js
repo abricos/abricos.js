45d18262b2f58c66d27184ec74ab0776ac5fafed
Alexander Kuzmin <roosit@abricos.org>
2013-12-06 15:28:42 +0400

    Набросок фреймворка

---
+++ src/abricos.js
@@ -0,0 +1,20 @@
+/*
+ *
+ *
+ */
+
+Abricos = Abricos || {};
+
+(function(){
+
+	var gel = function(nm){ return document.getElementById(nm); };
+	
+    // TEMPLATES[name] = document.getElementById(name + '-template').innerHTML;
+	
+	
+	var Component = function(modName, compName, cfg){
+		
+	};
+	
+
+})();
---
+++ test/modules/mymod1/helloworld.js
@@ -0,0 +1,8 @@
+var Component = new Abricos.Component();
+Component.requires = {
+	mod:[
+		{name: 'sys', files: ['container.js']}
+	]
+};
+Component.entryPoint = function(NS){
+}
